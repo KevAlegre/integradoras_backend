1a99d5f0d8b022d25494bfab72b2a07b91f65ea2
KevAlegre <kovinalegre@gmail.com>
2024-05-14 18:39:07 -0300

    Primer práctica integradora

---
+++ Primera-practica-integradora/src/app.js
@@ -0,0 +1,33 @@
+//Primera práctica integradora
+
+import express from "express";
+import handlebars from "express-handlebars"
+import socketProducts from "./server/socketProducts.js";
+import { Server } from "socket.io";
+import productsRouter from "./routes/products.router.js";
+import cartsRouter from "./routes/carts.router.js"
+import viewsRouter from "./routes/views.router.js";
+import __dirname from "./utils.js";
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+
+//Handlebars
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
+
+app.use("/", productsRouter);
+app.use("/", cartsRouter);
+app.use("/", viewsRouter);
+
+const httpServer = app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+const socketServer = new Server(httpServer);
+
+socketProducts(socketServer);
